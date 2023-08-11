66af55af0761ea22986b6f38080f10fb78925aa2
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-11 07:57:47 -0700

    hey

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nu = 0;
+
+	while (nu < 10)
+	{
+		putchar(nu + '0');
+		nu++;
+	}
+	return (0);
+}
