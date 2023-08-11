01888dcf2942d298a2fca0c9d51cf806b4c6ed4c
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-11 07:10:10 -0700

    hey

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+	return (0);
+}
