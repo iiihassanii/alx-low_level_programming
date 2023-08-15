cbd7a385c2ab57f1bcee8ceba7cab6ee56a89e86
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 06:00:18 -0700

    new day new tasks!

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * times_table - Description: A C program that print
+ *
+ */
+
+void times_table(void)
+{
+	int i , j, k;
+
+	for (i = 0 ;i < 10; i++)
+	{
+		for (j = 0 ;j < 10 ;j++)
+		{
+			k = i * j;
+			if (k > 9)
+			{
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
+			}
+			else
+				_putchar(k + 48);
+		}
+		_putchar('\n');
+	}
+}
