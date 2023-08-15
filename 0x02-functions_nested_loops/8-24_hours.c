b970202cf0239be5d2e68388a5ce0e0205783725
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 05:41:43 -0700

    new day new tasks!

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * jack_bauer - Description: A C program that print
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k = 0;
+
+	for (i=0 ; i < 24 ; i++)
+	{
+		if (i < 10)
+			_putchar('0');
+		_putchar(i);
+		_putchar(':');
+		for (j=0 ; j < 60 ; j++)
+		{
+			if (j < 10)
+				_putchar('0');
+			_putchar(j);
+		}
+	}
+}
