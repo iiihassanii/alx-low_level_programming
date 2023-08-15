3ef9b41557690e5c1d34f511fe2902a460642b04
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 06:53:30 -0700

    new day new tasks!

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _abs - Description: A C program that print
+ *
+ * @n1: input
+ * @n2 : inpdfff
+ *
+ * Return: 1 if c is letter, lowercase or uppercase - 0 otherwise
+ */
+
+int add(int n1, int n2)
+{
+	n1 = n1 + n2;
+	if (n1 > 9)
+	{
+		_putchar((n1 / 10) + 48);
+		_putchar((n1 % 10) + 48);
+	}
+	else
+		_putchar(n1 + 48);
+}
