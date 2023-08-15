cfa4ba5bee1c8520ebc3f7d7a05642a8ea205ccd
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 05:21:35 -0700

    new day new tasks!

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_last_digit - Description: A C program that print
+ *
+ * @n: input
+ *
+ * Return: 1 if c is letter, lowercase or uppercase - 0 otherwise
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
