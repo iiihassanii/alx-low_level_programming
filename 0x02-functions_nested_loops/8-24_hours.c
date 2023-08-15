cdefa328220d460f7202b0146775a10dbdc41b6a
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 05:51:18 -0700

    new day new tasks!

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,16 +12,15 @@ void jack_bauer(void)
 
 	for (i = 0 ; i < 24 ; i++)
 	{
-		if (i < 10)
-			_putchar('0');
-		_putchar(i + '0');
-		_putchar(':');
 		for (j = 0 ; j < 60 ; j++)
 		{
-			if (j < 10)
-				_putchar('0');
-			_putchar(j + '0');
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar(':');
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar('\n');
 		}
-		_putchar('\n');
+		
 	}
 }
