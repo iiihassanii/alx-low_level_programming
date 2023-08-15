0c0d9ebdfc0e14ebfd99cf1f706b5731742d0b6d
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 05:17:21 -0700

    new day new tasks!

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _abs - Description: A C program that print
+ *
+ * @n: input
+ *
+ * Return: 1 if c is letter, lowercase or uppercase - 0 otherwise
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n *= -1;
+	return (n);
+}
