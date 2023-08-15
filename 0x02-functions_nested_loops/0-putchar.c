536e5218e15e6a0298e0e5371c7100798be734a9
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 02:28:47 -0700

    new day new tasks!

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
