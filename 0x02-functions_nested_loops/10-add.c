178406ac7d8ec2c1ab364e09f844fec9e93bed2d
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 06:59:07 -0700

    new day new tasks!

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -14,12 +14,6 @@ int add(int n1, int n2)
 {
 	int sum;
 	sum = n1 + n2;
-	if (sum > 9)
-	{
-		_putchar((sum / 10) + 48);
-		_putchar((sum % 10) + 48);
-	}
-	else
-		_putchar(sum + 48);
-	return (0);
+
+	return (sum);
 }
