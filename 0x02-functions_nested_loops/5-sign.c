d569ba4c890a4a971ed1b9b609d74ad3ffeee819
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 04:55:34 -0700

    new day new tasks!

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,23 +13,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
+		_putchar(43);
 		return (1);
 	}
 	if (n == 0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
+		_putchar(48);
 		return (0);
 	}
 	if (n < 0)
 	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
+		_putchar(45);
 		return (-1);
 	}
+
 }
+
