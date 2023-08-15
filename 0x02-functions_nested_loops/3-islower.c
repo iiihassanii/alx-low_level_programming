ea8d29d48e7a0ee9a4d24ed782e600225c69b5b7
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 04:27:34 -0700

    new day new tasks!

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,11 +4,14 @@
 /**
  * _islower - Description: A C program that print
  *
+ * @c: input
+ *
+ * Return: 1 if c is lowercase - 0 somthind else
  */
 
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 
