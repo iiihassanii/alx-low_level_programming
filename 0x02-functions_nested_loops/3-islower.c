c9bf638d616c801a121dc3fbf34cc712893d050c
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 04:22:09 -0700

    new day new tasks!

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - Description: A C program that print
+ *
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	return (1);
+	return (0);
+
+}
