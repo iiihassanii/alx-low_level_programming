6e08e7efc30e414e40f2bd63bc0cafe792f50ee4
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-15 03:46:23 -0700

    new day new tasks!

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
