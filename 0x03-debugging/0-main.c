23bb7b8796283ddd50619bff3a7a24a089ced47a
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-16 03:46:51 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	if (positive_or_negative(0) == "0 is zero")
+		printf("0 is zero");
+}
