9c30e1608f699cdee5e54845541bd5cd906e9fe0
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-17 09:32:40 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Description: A C program that print
+ *
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i ; i < 10; i++)
+		_putchar(i + '48');
+	_PUTCHAR('\n');
+}
