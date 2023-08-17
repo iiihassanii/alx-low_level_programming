c89687d66cb432b6e1c2792d8831e517128c09b3
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-17 10:46:08 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - Description: A C program that print
+ *
+ * @n: input
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
