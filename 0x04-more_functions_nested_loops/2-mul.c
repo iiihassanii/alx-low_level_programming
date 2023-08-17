9610b26e9742025636515d591fe89fc2beaf493a
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-17 09:24:55 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multibly 2 int
+ * @a: first input
+ * @b: second input
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
