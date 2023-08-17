25014cb6fdf6fd9b5e765ab909c39d7a0a8ee421
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-17 08:32:28 -0700

    functions and loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * _islower - Description: A C program that print
+ *
+ * @c: input
+ *
+ * Return: 1 if c is lowercase - 0 somthind else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
