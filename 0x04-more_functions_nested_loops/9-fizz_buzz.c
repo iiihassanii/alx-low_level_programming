b3fed61c6fc467573d1ffb96579f71d34f7634f1
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-17 12:04:31 -0700

    functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,7 +20,7 @@ int main(void)
 			printf("Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
