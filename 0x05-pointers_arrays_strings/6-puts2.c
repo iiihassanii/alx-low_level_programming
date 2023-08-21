b91ae38952edc79468f7548c76079bb3e1714994
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-21 06:16:59 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *@str: pointer for the number
+ * Return: Void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
