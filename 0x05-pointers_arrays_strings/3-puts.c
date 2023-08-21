d1976c0744ba5120520ac649c637ddd6101996bd
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-21 03:59:56 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line
+ *@str: pointer for the string
+ * Return: Void
+ */
+
+void _puts(char *str);
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+}
