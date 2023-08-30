ad6fda31d75265ce943ff9bb304cefb88b4badd2
iiihassanii <hassanabdelazim.max@gmail.com>
2023-08-30 04:28:18 -0700

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - power
+ * @x:int
+ * @y:int
+ * Return:int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		if (y > 1)
+		{
+			x *=  _pow_recursion(x, y - 1);
+		}
+	return (x);
+}
