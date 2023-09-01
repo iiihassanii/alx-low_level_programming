27daecc69610a171aa53711c97bb0f7d90d97c2a
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-01 00:22:53 -0700

    hey

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print name if the program
+ * @argc : int
+ * @argv: list
+ * Return : 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+hey 0x0A-argc_argv
