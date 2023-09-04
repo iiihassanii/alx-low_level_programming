2762a83ea135666bea78aab4aba7362245b17249
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-04 09:31:45 -0700

    hey

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -33,10 +33,10 @@ int **alloc_grid(int width, int height)
 			free(array);
 			return (NULL);
 		}
-		for (j = 0; j < width; j++)
-		{
-			array[i][j] = 0;
-		}
 	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+
 	return (array);
 }
