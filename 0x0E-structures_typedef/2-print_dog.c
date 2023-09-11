47cce5b53157368bd3f0e71ebf5d8dc6434b9c68
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-11 03:48:43 -0700

    Structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,11 +16,9 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-
 		printf("Age: %.6f\n", d->age);
-
 		if (d->owner != NULL)
-			 printf("Owner: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)\n");
 	}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	unsigned int nameLen, ownerLen, i;
+
+	/*make sure nothing is empty*/
+	if (name == NULL || owner == NULL || age < 0)
+		return (NULL);
+	/*malloc for dog record*/
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	/*malloc for name in dog record & check*/
+	nameLen = strlen(*name);
+	dog->name = malloc(sizeof(char) * nameLen);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	/* save name*/
+	for (i = 0; i <= nameLen; i++)
+		dog->name[i] = name[i];
+	/*save dog's age*/
+	dog->age = age;
+
+	/*malloc for owner's recoed*/
+	ownerLen = strlen(*owner);
+	dog->owner = malloc(sizeof(char) * ownerLen);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	/* save owner*/
+	for (i = 0; i <= ownerLen; i++)
+		dog->owner[i] = owner[i];
+	/*END*/
+	return (dog);
+}
