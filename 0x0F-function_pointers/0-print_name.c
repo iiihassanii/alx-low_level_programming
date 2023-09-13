e47559707d10327f45953d5428b11ac7f9243e80
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-13 03:22:54 -0700

    pointer -> function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ *
+ * @name: string name
+ * @f: fun
+ *
+ * Return: VOID
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
