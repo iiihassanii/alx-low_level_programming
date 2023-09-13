ae59c355fc3afa4b4676024d84cd097756811f21
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-13 05:58:06 -0700

    pointer -> function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,9 +17,9 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-    int i;
+	int i;
 
-    while (i < 5)
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
