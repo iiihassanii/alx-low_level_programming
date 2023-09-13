e9e4aded113b88e50ea5138b8ea88a7b430fbe6d
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-13 05:56:18 -0700

    pointer -> function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-
+#include <string.h>
 
 /**
   * get_op_func - get functions
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
     while (i < 5)
 	{
-		if (s == ops[i].op)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 
 		i++;
