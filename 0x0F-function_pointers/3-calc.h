bb665609348d33fbe666fea03145f92877221dba
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-13 06:13:24 -0700

    pointer -> function

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -12,6 +12,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
