4b5adca3356c0efd42c2e05f74b39376e2a00657
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-21 09:33:07 -0700

    FINISH

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void before_main() __attribute__((constructor));
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+int main()
+{
+	return (0);
+}
