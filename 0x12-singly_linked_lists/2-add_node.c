04f9a64caea3d70bf9caeaf794ea17fd59b0b4c3
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-21 04:28:48 -0700

    hey

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,27 +1,6 @@
 #include "lists.h"
 
 /**
- * _len- len
- * @s: ...
- * Return: ....
- */
-
-
-int _len(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-	{
-		i++;
-	}
-	return (i);
-}
-
-
-/**
  * add_node- add
  * @head: ...
  * @str: .....
