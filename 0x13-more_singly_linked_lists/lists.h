8f2c33c173280d1460bcf4945d6abba5addf5e93
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-26 14:33:27 -0700

    hello

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,6 +20,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
