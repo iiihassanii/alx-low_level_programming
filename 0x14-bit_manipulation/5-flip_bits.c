df5227e8cff9b193a8b321ec030741769404fcbc
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-28 09:36:02 -0700

    hey

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - ....................
+ * @n: .........
+ * @m: ...........
+ * Return: .....
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff;
+	unsigned int count = 0;
+
+	diff = n ^ m;
+	while (diff)
+	{
+		counter++;
+		diff &= (diff - 1);
+	}
+	return (count);
+}
