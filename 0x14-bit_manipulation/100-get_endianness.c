a045f3d465ef748104f4425039c0fa3f3b01cafa
iiihassanii <hassanabdelazim.max@gmail.com>
2023-09-28 09:46:51 -0700

    hey

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,10 +4,12 @@
  * get_endianness - ....
  * Return: .......
  */
+
 int get_endianness(void)
 {
 	unsigned int i = 1;
-	char *c = (char*)&i;
+	char *c = (char *) &i;
+
 	if (*c)
 		return (1);
 	else
