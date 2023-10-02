0bb4c170e3a3aeaf204d6f4a52d132ebad3c2426
iiihassanii <hassanabdelazim.max@gmail.com>
2023-10-02 07:23:37 -0700

    waay

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,27 +20,27 @@ int main(int ac, char **av)
 	char buff[1024];
 
 	if (ac != 3)
-		dprintf(fileno(stderr), "Usage: cp file_from file_to\n"), exit(97);
+		fprintf(stderr, "Usage: cp file_from file_to\n"), exit(97);
 	ofile_from = open(av[1], O_RDONLY); /*open file from "the first file" */
 	if (ofile_from == -1)
-		dprintf(fileno(stderr), "Error: Can't read from file %s\n", av[1]), exit(98);
+		fprintf(stderr, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	ofile_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, PER);
 	if (ofile_to == -1)
-		dprintf(fileno(stderr), "Error: Can't write to %s\n", av[2]), exit(99);
+		fprintf(stderr, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	while ((rbuff =  read(ofile_from, buff, 1024)) > 0)
 		if (write(ofile_to, buff, rbuff) != rbuff)
-			dprintf(fileno(stderr), "Error: Can't read from file %s\n", av[1]), exit(98);
+			fprintf(stderr, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	if (rbuff == -1)
-		dprintf(fileno(stderr), "Error: Can't read from file %s\n", av[1]), exit(98);
+		fprintf(stderr, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	ofile_from = close(ofile_from);
 	ofile_to = close(ofile_to);
 	if (ofile_from)
-		dprintf(fileno(stderr), "Error: Can't close fd %d\n", ofile_from), exit(100);
+		fprintf(stderr, "Error: Can't close fd %d\n", ofile_from), exit(100);
 	if (ofile_to)
-		dprintf(fileno(stderr), "Error: Can't close fd %d\n", ofile_to), exit(100);
+		fprintf(stderr, "Error: Can't close fd %d\n", ofile_to), exit(100);
 	return (0);
 }
