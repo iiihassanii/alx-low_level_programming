57f1e82fa8d034900b0f20dd93d22635d41405fe
iiihassanii <hassanabdelazim.max@gmail.com>
2023-10-02 07:56:02 -0700

    way

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,11 +24,13 @@ int main(int ac, char **av)
 	ofile_from = open(av[1], O_RDONLY); /*open file from "the first file" */
 	if (ofile_from == -1)
 		fprintf(stderr, "Error: Can't read from file %s\n", av[1]), exit(98);
-
 	ofile_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, PER);
 	if (ofile_to == -1)
-		fprintf(stderr, "Error: Can't write to %s\n", av[2]), exit(99);
-
+	{
+		fprintf(stderr, "Error: Can't write to %s\n", av[2]);
+		close(ofile_from);
+		exit(99);
+	}
 	lenr = 1024;
 	while (lenr == 1024)
 	{
