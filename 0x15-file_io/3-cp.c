c5569e296a99dd88c6bb04145f8f7255c45a9eca
iiihassanii <hassanabdelazim.max@gmail.com>
2023-10-02 07:04:09 -0700

    waaay

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+#define PER (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 /**
  * main - check the code
  * @ac: ........
@@ -15,39 +17,27 @@ int main(int ac, char **av)
 	char buff[1024];
 
 	if (ac != 3)
-	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		fprintf(stderr, "Usage: cp file_from file_to\n"), exit(97);
 	ofile_from = open(av[1], O_RDONLY); /*open file from "the first file" */
 	if (ofile_from == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	ofile_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0777);
+		fprintf(stderr, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	ofile_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, PER);
 	if (ofile_to == -1)
-	{
-		fprintf(stderr, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		fprintf(stderr, "Error: Can't write to %s\n", av[2]), exit(99);
+
 	while ((rbuff =  read(ofile_from, buff, 1024)) > 0)
 		if (write(ofile_to, buff, rbuff) != rbuff)
-				{
-					fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
-					exit(98);
-				}
+			fprintf(stderr, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	if (rbuff == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		fprintf(stderr, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	ofile_from = close(ofile_from);
 	ofile_to = close(ofile_to);
-	if (ofile_from || ofile_to)
-	{
-		fprintf(stderr, "Error: Can't close fd %d\n", -1);
-		exit(100);
-	}
+	if (ofile_from)
+		fprintf(stderr, "Error: Can't close fd %d\n", ofile_from), exit(100);
+	if (ofile_to)
+		fprintf(stderr, "Error: Can't close fd %d\n", ofile_from), exit(100);
 	return (0);
 }
