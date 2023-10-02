1a981f192353d476c8715ce9b121284207f6d19b
iiihassanii <hassanabdelazim.max@gmail.com>
2023-10-02 04:50:11 -0700

    waaaay

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	/*get len of content -> will pass it letter to write function */
 	if (text_content)
-		while (text_content)
+		while (text_content[len])
 			len++;
 	/*start to work on file*/
 	/*first open the file */
