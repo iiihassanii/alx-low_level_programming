ccb73894b7ff296a448f83033d89076466ec16e0
iiihassanii <hassanabdelazim.max@gmail.com>
2023-10-02 05:12:31 -0700

    yooo

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,6 +7,7 @@
  *
  * Return:....
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
@@ -20,8 +21,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 
-	o = open(filename, O_WRONLY | O_APPEND);
+	o = open(filename, O_WRONLY | O_APPEND, 0600);
 	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
 
 	close(o);
 	return (1);
