432f255a7d2e8dea0f07d562e941465320b849e3
iiihassanii <hassanabdelazim.max@gmail.com>
2023-10-03 03:18:45 -0700

    way

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -49,40 +49,27 @@ int main(int argc, char *argv[])
 	int close_in, close_out;
 	char buffer[MAXSIZE];
 
-	/*if arguments are not 3*/
 	if (argc != 3)
 		exit_(97, NULL, 0);
-
-	/*sets file descriptor for copy from file*/
 	file_in = open(argv[1], O_RDONLY);
 	if (file_in == -1)
 		exit_(98, argv[1], 0);
-
-	/*sets file descriptor for copy to file*/
 	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (file_out == -1)
 		exit_(99, argv[2], 0);
-
-	/*reads file_in as long as its not NULL*/
 	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
 	{
 		if (read_stat == -1)
 			exit_(98, argv[1], 0);
-
-		/*copy and write contents to file_out*/
 		write_stat = write(file_out, buffer, read_stat);
 		if (write_stat == -1)
 			exit_(99, argv[2], 0);
 	}
-	/*close the files*/
-
 	close_in = close(file_in);
 	if (close_in == -1)
 		exit_(100, NULL, file_in);
-
 	close_out = close(file_out);
 	if (close_out == -1)
 		exit_(100, NULL, file_out);
-
 	return (0);
 }
