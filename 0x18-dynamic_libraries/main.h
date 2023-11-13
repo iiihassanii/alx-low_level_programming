b86bea1dce2107313234931270cac47dcd8866c3
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-13 16:08:34 +0300

    ?

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,4 +1,4 @@
-#ifndef NAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -25,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
