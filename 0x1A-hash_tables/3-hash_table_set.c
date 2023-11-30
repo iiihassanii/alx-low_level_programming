6996a43b3a2ed95c80ee7ce9466e40ddca8dd8c4
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-30 18:19:31 +0300

    ALL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,53 +1,68 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
+ * create_node - Creates a new hash node.
+ * @key: The key for the node.
+ * @value: The value associated with the key.
  *
- * Return: Upon failure - 0.
- *         Otherwise - 1.
+ * Return: A pointer to the newly created hash node,
+ *         or NULL if an error occurred.
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (!node)
+		return (NULL);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+
+	return (node);
+}
+/**
+ * hash_table_set - Inserts or updates a key-value pair in a hash table.
+ * @ht: The hash table to insert or update the key-value pair.
+ * @key: The key for the key-value pair.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if the operation is successful, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index, i;
+	unsigned long int index;
+	hash_node_t *node, *check_node, *collision;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	if (key == NULL || *key == '\0' || ht == NULL || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	/*Insetr key for the first time*/
+	if (ht->array[index] == NULL)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		node = create_node(key, value);
+		ht->array[index] = node;
+		/*printf("Insert at--->\t%s value[%s]\n", key, node->value);*/
+		return (1);
+	}
+	/*Update node*/
+	check_node = ht->array[index];
+	while (check_node != NULL)
+	{
+		if (strcmp(check_node->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			free(check_node->value), check_node->value = NULL;
+			check_node->value = strdup(value);
+			/*printf("Update--->\t%s\n", key);*/
 			return (1);
 		}
+		collision = check_node; /**/
+		check_node = check_node->next;
 	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(value_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	/*Handling Collisions*/
+	node = create_node(key, value);
+	collision->next = node;
+	/*printf("Handling Collisions %s--->\t%s\n", collision->key, key);*/
 
 	return (1);
 }
