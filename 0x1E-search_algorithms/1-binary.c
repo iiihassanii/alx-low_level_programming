f72e40640fe3424596cd76bae73f3c255e777239
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-12 19:31:33 +0300

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,14 @@
 #include "search_algos.h"
 #include <math.h>
+
+/**
+ * binary_search - binary_search algos
+ * @array: pointer to the first element of the array to search in.
+ * @size: size is the number of elements in array.
+ * @value: And value is the value to search for
+ * Return: If value is not present in array or if
+ * array is NULL, your function must return -1
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0, right = size - 1, mid;
